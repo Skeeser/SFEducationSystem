e0f1581735a3529cfb56b0636c5211a24f5c422a
keeser <1326658417@qq.com>
2023-04-30 00:38:28 +0800

    写速刷局刷注释

--- 终端/ESP-12F/queedle/include/epd.h
+++ 终端/ESP-12F/queedle/include/epd.h
@@ -31,6 +31,7 @@ void EPD_WhiteScreen_ALL_Clean(void);
 void EPD_Dis_Full(const uint8_t *DisBuffer);
 void EPD_Dis_Part(int xStart, int xEnd, int yStart, int yEnd, const uint8_t *DisBuffer);
 // 向ram中写值 全刷--00 速刷--10 局刷--11
+// 全刷能确定底图，速刷和局刷都是覆盖在底图上面， 而01则是将底图局刷，由于时间慢，暂时没什么用
 void EPD_Transfer_Full_BW(const uint8_t *DisBuffer);                                          // 0
 void EPD_Transfer_Part(int xStart, int xEnd, int yStart, int yEnd, const uint8_t *DisBuffer); // 1
 // 基本功能
